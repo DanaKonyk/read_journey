48808ccb89e37707ee15c2c43be69394f54fc7c5
Dana <mitsunokehl@gmail.com>
2024-05-20 19:21:07 +0300

    routes settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import HomePage from '../pages/Home/HomePage';
+import RegistrationPage from '../pages/Registration/RegistrationPage';
+import LoginPage from '../pages/Login/LoginPage';
+import LibraryPage from '../pages/Library/LibraryPage';
+import ReadingPage from '../pages/Reading/ReadingPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/library" element={<LibraryPage />} />
+          <Route path="/reading" element={<ReadingPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
+
+export default App;
